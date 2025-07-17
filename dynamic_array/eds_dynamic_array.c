009944c1ec27aff9c80457076dbbff8676f6e8ad
edudscrc <eduardo.sa.coelho@gmail.com>
2025-07-17 00:04:20 -0300

    much better error handle, refactored all functions and added a little, library name will be EdS (Eduardo de Sá)

---
+++ dynamic_array/eds_dynamic_array.c
@@ -0,0 +1,223 @@
+#include "eds_dynamic_array.h"
+
+EdS_dynamic_array_t *EdS_dynamic_array_new(size_t initial_capacity) {
+    if (initial_capacity == 0) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <EdS_dynamic_array_new>.\n");
+        return NULL;
+    }
+
+    EdS_dynamic_array_t *arr = malloc(sizeof(EdS_dynamic_array_t));
+    if (arr == NULL) {
+        fprintf(stderr, "ERROR: `malloc` returned NULL in function <EdS_dynamic_array_new>.\n");
+        return NULL;
+    }
+
+    arr->data = malloc(sizeof(int) * initial_capacity);
+    if (arr->data == NULL) {
+        fprintf(stderr, "ERROR: `malloc` returned NULL in function <EdS_dynamic_array_new>.\n");
+        return NULL;
+    }
+
+    arr->capacity = initial_capacity;
+    arr->size = 0;
+
+    return arr;
+}
+
+int EdS_dynamic_array_append(EdS_dynamic_array_t *arr, int value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <append>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (arr->size == arr->capacity) {
+        int *resized_data = reallocarray(arr->data, arr->capacity * 2, sizeof(int));
+        if (resized_data == NULL) {
+            fprintf(stderr, "ERROR: `reallocarray` returned NULL in function <append>.\n");
+            return EDS_RETURN_ERROR;
+        }
+        arr->data = resized_data;
+        arr->capacity *= 2;
+    }
+
+    arr->data[arr->size++] = value;
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_insert(EdS_dynamic_array_t *arr, size_t index, int value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <insert>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (index > arr->size) {
+        fprintf(stderr, "ERROR: invalid <index> in function <insert>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (arr->size == arr->capacity) {
+        int *resized_data = reallocarray(arr->data, arr->capacity * 2, sizeof(int));
+        if (resized_data == NULL) {
+            fprintf(stderr, "ERROR: `reallocarray` returned NULL in function <insert>.\n");
+            return EDS_RETURN_ERROR;
+        }
+        arr->data = resized_data;
+        arr->capacity *= 2;
+    }
+
+    for (size_t i = arr->size; i > index; --i) {
+        arr->data[i] = arr->data[i-1];
+    }
+
+    arr->data[index] = value;
+    arr->size++;
+
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_pop(EdS_dynamic_array_t *arr, int *removed_value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <pop>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (arr->size == 0) {
+        fprintf(stderr, "ERROR: parameter `arr` is empty in function <pop>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    arr->size--;
+
+    if (removed_value != NULL) {
+        *removed_value = arr->data[arr->size];
+    }
+
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_remove(EdS_dynamic_array_t *arr, int value, int *removed_value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <remove_value>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    for (size_t i = 0; i < arr->size; ++i) {
+        if (arr->data[i] == value) {
+            return EdS_dynamic_array_remove_at(arr, i, removed_value);
+        }
+    }
+
+    fprintf(stderr, "ERROR: `value` %d not found in `arr`.\n", value);
+    return EDS_RETURN_ERROR;
+}
+
+int EdS_dynamic_array_remove_at(EdS_dynamic_array_t *arr, size_t index, int *removed_value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <remove_at>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (index >= arr->size) {
+        fprintf(stderr, "ERROR: invalid <index> in function <remove_at>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (removed_value != NULL) {
+        *removed_value = arr->data[index];
+    }
+
+    for (size_t i = index; i < arr->size - 1; ++i) {
+        arr->data[i] = arr->data[i+1];
+    }
+
+    arr->size--;
+
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_free(EdS_dynamic_array_t *arr) {
+    if (arr == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <destroy_dynamic_array>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    free(arr->data);
+    arr->data = NULL;
+    arr->size = 0;
+    arr->capacity = 0;
+
+    free(arr);
+    arr = NULL;
+
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_clear(EdS_dynamic_array_t *arr) {
+    if (arr == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <clear>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    arr->size = 0;
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_get(const EdS_dynamic_array_t *arr, size_t index, int *value) {
+    if (arr == NULL || arr->data == NULL || value == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <get>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (index >= arr->size) {
+        fprintf(stderr, "ERROR: invalid <index> in function <get>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    *value = arr->data[index];
+    return EDS_RETURN_SUCCESS;
+}
+
+int EdS_dynamic_array_set(EdS_dynamic_array_t *arr, size_t index, int value, int *old_value) {
+    if (arr == NULL || arr->data == NULL) {
+        fprintf(stderr, "ERROR: NULL pointer passed in function <set>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (index >= arr->size) {
+        fprintf(stderr, "ERROR: invalid <index> in function <set>.\n");
+        return EDS_RETURN_ERROR;
+    }
+
+    if (old_value != NULL) {
+        *old_value = arr->data[index];
+    }
+
+    arr->data[index] = value;
+    return EDS_RETURN_SUCCESS;
+}
+
+void EdS_dynamic_array_print_info(EdS_dynamic_array_t *arr) {
+    if (arr == NULL) {
+        printf("ERROR: parameter `arr` is NULL in function <print_info>.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Capacity: %zu\n", arr->capacity);
+    printf("Size: %zu\n", arr->size);
+}
+
+void EdS_dynamic_array_traverse(EdS_dynamic_array_t *arr) {
+    if (arr == NULL) {
+        printf("ERROR: parameter `arr` is NULL in function <traverse>.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (arr->data == NULL) {
+        printf("ERROR: parameter `arr->data` is NULL in function <traverse>.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (size_t i = 0; i < arr->size; ++i) {
+        printf("%d\n", arr->data[i]);
+    }
+}
--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -1,39 +1,38 @@
-#include "dynamic_array.h"
+#include "eds_dynamic_array.h"
 
 #define INITIAL_CAPACITY 5
 #define AMOUNT_TO_INSERT 30
 
 int main(void) {
-    DynamicArray my_array;
-    init_dynamic_array(&my_array, INITIAL_CAPACITY);
+    EdS_dynamic_array_t* my_array = EdS_dynamic_array_new(INITIAL_CAPACITY);
 
     for (int i = 0; i < AMOUNT_TO_INSERT; ++i) {
-        append(&my_array, i * 2);
+        EdS_dynamic_array_append(my_array, i * 2);
     }
 
-    print_info(&my_array);
+    EdS_dynamic_array_print_info(my_array);
 
-    traverse(&my_array);
+    EdS_dynamic_array_traverse(my_array);
 
-    remove_at(&my_array, 4);
-    remove_at(&my_array, 14);
+    EdS_dynamic_array_remove_at(my_array, 4, NULL);
+    EdS_dynamic_array_remove_at(my_array, 14, NULL);
 
-    remove_value(&my_array, 42);
+    EdS_dynamic_array_remove(my_array, 42, NULL);
 
-    insert_at(&my_array, 100, 8);
-    insert_at(&my_array, 9767, my_array.size);
+    EdS_dynamic_array_insert(my_array, 8, 100);
+    EdS_dynamic_array_insert(my_array, my_array->size, 9767);
 
-    traverse(&my_array);
+    EdS_dynamic_array_traverse(my_array);
 
-    remove_last(&my_array);
-    remove_last(&my_array);
-    remove_last(&my_array);
+    EdS_dynamic_array_pop(my_array, NULL);
+    EdS_dynamic_array_pop(my_array, NULL);
+    EdS_dynamic_array_pop(my_array, NULL);
 
-    print_info(&my_array);
+    EdS_dynamic_array_print_info(my_array);
 
-    traverse(&my_array);
+    EdS_dynamic_array_traverse(my_array);
 
-    destroy_dynamic_array(&my_array);
+    EdS_dynamic_array_free(my_array);
 
     return 0;
 }
